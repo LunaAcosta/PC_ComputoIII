867d3a8550bcff523a2708d19c0770bbc3d5b5ca
LunaAcosta <lunaacosta300@gmail.com>
2023-05-28 22:58:02 -0600

    Se corrigen errores

--- Semana16/laravelAuth/routes/web.php
+++ Semana16/laravelAuth/routes/web.php
@@ -17,7 +17,6 @@ use App\Http\Controllers\StudentController;
 Route::get('/', function () {
     return view('auth.login');
 });
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
